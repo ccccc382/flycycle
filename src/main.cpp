163bf0e39a680cec0677aec0fef6674b9167fc70
ccccc382 <ccccc382@gmail.com>
2017-09-06 19:29:00 +0200

    v0.1

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,40 @@ Joystick_ joystick;
 
 #define PotPin 4
 #define IrPin 5
-#define RightBrake 6
-#define LeftBrake 7
+#define ReedPin 6
+#define RightBrake 7
+#define LeftBrake 8
 
 
-
-int analogue
 
 void setup() {
-    // put your setup code here, to run once:
+    //activate internal pull ups
+    pinMode(RightBrake, INPUT_PULLUP);
+    pinMode(LeftBrake, INPUT_PULLUP);
+
+    //initialize joystick library
+    joystick.begin();
 }
 
+//function to set button states
+void Button(int input) {
+  if (input == 1) {
+    joystick.pressButton(input);
+    joystick.releaseButton(input);
+  }
+  else {
+    joystick.pressButton(input);
+  }
+}
+
+
+
+
 void loop() {
-    // put your main code here, to run repeatedly:
+
+  joystick.setXAxis(analogRead(PotPin));
+  joystick.setYAxisT(analogRead(IrPin));
+  joystick.setZAxis(analogRead(ReedPin));
+  Button(RightBrake);
+  Button(LeftBrake);
 }
