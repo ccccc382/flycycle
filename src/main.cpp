869b00cd6fc31694865ba82110371e117c826b8a
ccccc382 <ccccc382@gmail.com>
2017-09-06 18:49:03 +0200

    v0.01

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,17 @@
 #include <Arduino.h>
+#include <joystick.h>
+
+// Create the Joystick
+Joystick_ joystick;
+
+#define PotPin 4
+#define IrPin 5
+#define RightBrake 6
+#define LeftBrake 7
+
+
+
+int analogue
 
 void setup() {
     // put your setup code here, to run once:
